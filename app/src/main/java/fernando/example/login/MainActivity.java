c960f32df57dbb6bb952666c90471ea9a1f8a868
luissfernando <82791279+luissfernando@users.noreply.github.com>
2021-08-09 22:51:18 -0500

    tiendita
    tiendita

---
+++ app/src/main/java/fernando/example/login/MainActivity.java
@@ -0,0 +1,26 @@
+package fernando.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv_registrar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_registrar = findViewById(R.id.tv_registrar);
+        tv_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentReg = new Intent(MainActivity.this, Registro.class);
+                MainActivity.this.startActivity(intentReg);
+            }
+        });
+    }
+}
