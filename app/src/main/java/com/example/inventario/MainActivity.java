4f6f662d08f37f40cede30ecb006b1ccf4b623ad
luissfernando <82791279+luissfernando@users.noreply.github.com>
2021-08-10 01:15:42 -0500

    lregistro
    registro

--- app/src/main/java/com/example/inventario/MainActivity.java
+++ app/src/main/java/com/example/inventario/MainActivity.java
@@ -6,14 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv_registrar;
+    Button usuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tv_registrar = findViewById(R.id.boton_registrar);
+        tv_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentReg = new Intent(MainActivity.this, Registro.class);
+                MainActivity.this.startActivity(intentReg);
+            }
+        });
     }
+
     public void sig(View v){
         Intent i=new Intent(this,tienda.class);
         startActivity(i);
